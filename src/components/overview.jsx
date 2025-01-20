c144087a60966614f8e8382d8adaf32fae877201
Rolamaalouf <roula.maalouf@hotmail.fr>
2025-01-20 12:42:50 +0200

    overwrite old installation of bootstrap

--- src/components/overview.jsx
+++ src/components/overview.jsx
@@ -3,7 +3,7 @@
 import React from 'react';
 import createHabit from './createHabit';
 import { useSelector, useDispatch } from 'react-redux';
-import {Row, Col, Listgroup, Button } from 'bootstrap';
+import {Row, Col, Listgroup, Button } from 'react-bootstrap';
 import { useDispatch, useSelector } from 'react-redux';
 import {deleteHabit} from '../features/habitSlice';
 
